67863a87e712879cfdc379ea7a81aaafb8094020
dan <dan@dan-english.dev>
2023-04-23 13:57:17 +0100

    update

--- message/send.php
+++ message/send.php
@@ -1,42 +1,29 @@
 <?php
 require_once('../includes.php');
 
-$curl = curl_init();
+## Send an email message for testing webhooks
 
-curl_setopt_array($curl, [
-              CURLOPT_URL => $nylas_api_server.'/send',
-              CURLOPT_RETURNTRANSFER => true,
-              CURLOPT_ENCODING => '',
-              CURLOPT_MAXREDIRS => 10,
-              CURLOPT_TIMEOUT => 0,
-              CURLOPT_FOLLOWLOCATION => true,
-              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-              CURLOPT_CUSTOMREQUEST => 'POST',
-              CURLOPT_POSTFIELDS =>'{
-                "body" : "Just testing tracking webhooks!",
-                "subject": "Testing A webhook",
-                "to": [
-                    {
-                        "name": "Alex Li",
-                        "email": "alex.li@nylas.biz"
-                    }
+$token="TOKEN_WITH_EMAIL_SCOPES";
+
+$messagePayload =[
+                "body" => "Just testing tracking webhooks!",
+                "subject"=> "Testing A webhook",
+                "to" => [
+                    [
+                        "name" => "Alex Li",
+                        "email" => "alex.li@nylas.biz"
+                    ]
                 ],
-                "tracking": {
-                            "links": true,
-                            "opens": true,
-                            "thread_replies": true,
-                            "payload": "Any string you want!!"
-                        }
-              }',
-              CURLOPT_HTTPHEADER => [
-                'Content-Type: application/json',
-                'Authorization: Bearer '.$token
-              ],
-]);
+               "tracking" => [
+                            "links" => true,
+                            "opens" => true,
+                            "thread_replies" => true,
+                            "payload" => "Any string you want!!"
+                ]
 
-$response = curl_exec($curl);
+];
 
-curl_close($curl);
+$message = makeCurlRequest('/send', 'POST', $messagePayload, $token);
+print(json_encode($message));
 
-echo $response;
 ?>
